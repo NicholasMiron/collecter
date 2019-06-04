a9aaae335100c8744b6c8f58dc4965610f4790c3
Nicholas Miron <nicholasmiron@gmail.com>
2019-06-04 13:34:56 -0500

    inital

--- client/components/App.js
+++ client/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      greeting: "Hello World!"
+      greeting:   "Hello World!"
     }
   }
 
--- server/app.js
+++ server/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-// const db = require('../db/index.js');
-
 const app = express();
 
 app.use(morgan('dev'));
